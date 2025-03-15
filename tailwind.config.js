0f9d7dcc39f7e7cbabbe6d651be2c4f83d00df08
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-03-15 14:23:46 +0000

    final fix for dark mode

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    daisyui: {
-      themes: ['light'], // Only use light theme, no dark theme
-      darkTheme: false,   // Disable DaisyUI dark theme
-    },
-    darkMode: false,      // Ensure Tailwind doesn't apply dark mode automatically
-  }
